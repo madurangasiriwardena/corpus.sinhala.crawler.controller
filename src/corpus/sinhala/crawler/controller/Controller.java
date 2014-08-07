597c23a72dc8b4ab154feee6924f13e4dbb51a16
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2014-08-07 14:57:35 +0530

    adding test client

--- src/corpus/sinhala/crawler/controller/Controller.java
+++ src/corpus/sinhala/crawler/controller/Controller.java
@@ -1,17 +1,59 @@
 package corpus.sinhala.crawler.controller;
 
-public class Controller {
-	public static void main(String args[]){
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-		String jarPath = "/home/maduranga/workspace/corpus.sinhala.crowler/target/corpus.sinhala.crowler-1.0-SNAPSHOT-jar-with-dependencies.jar";
-		String start = "2010/1/1";
-		String end = "2010/1/2";
-		String host = "127.0.0.1";
-		int port = 12345;
-		String savePath = "/home/maduranga/data/";
+public class Controller {
+	public static void main(String args[]) throws IOException{
 		
-		Crawl c= new Crawl(jarPath, start, end, host, port, savePath);
-		Thread t = new Thread(c);
-		t.start();
+		int controllerPort = 11223;
+		
+		@SuppressWarnings("resource")
+		ServerSocket serverSocket = new ServerSocket(controllerPort);
+		System.out.println("Server> socket created");
+		
+		while(true){
+			Socket socket = serverSocket.accept();
+			System.out.println("Socket> accepted");
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			String msg;
+			String data = "";
+			while ((msg = input.readLine()) != null
+					&& !msg.equalsIgnoreCase("close")) {
+				if (!msg.equalsIgnoreCase("check")) {
+					System.out.println("Server> Input: " + msg);
+					data = msg;
+				}
+			}
+			System.out.println("Server> Socket closed");
+			socket.close();
+			
+			String[] temp = data.split("\\|");
+			
+			String jarPath = temp[0];
+			String start = temp[1];
+			String end = temp[2];
+			String host = temp[3];
+			int port = Integer.parseInt(temp[4]);
+			String savePath = temp[5];
+			
+//			String jarPath = "/home/maduranga/workspace/corpus.sinhala.crowler/target/corpus.sinhala.crowler-1.0-SNAPSHOT-jar-with-dependencies.jar";
+//			String start = "2010/1/1";
+//			String end = "2010/1/2";
+//			String host = "127.0.0.1";
+//			int port = 12345;
+//			String savePath = "/home/maduranga/data/";
+			
+			Crawl c= new Crawl(jarPath, start, end, host, port, savePath);
+			Thread t = new Thread(c);
+			t.start();
+		}
+		
+
+		
 	}
 }
--- src/corpus/sinhala/crawler/controller/Crawl.java
+++ src/corpus/sinhala/crawler/controller/Crawl.java
@@ -1,9 +1,10 @@
 package corpus.sinhala.crawler.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-
-public class Crawl implements Runnable{
+public class Crawl implements Runnable {
 	Process p;
 	String jarPath;
 	String start;
@@ -11,8 +12,9 @@ public class Crawl implements Runnable{
 	String host;
 	int port;
 	String savePath;
-	
-	public Crawl(String jarPath, String start, String end, String host, int port, String savePath){
+
+	public Crawl(String jarPath, String start, String end, String host,
+			int port, String savePath) {
 		this.jarPath = jarPath;
 		this.start = start;
 		this.end = end;
@@ -27,9 +29,25 @@ public class Crawl implements Runnable{
 			Server server = new Server(port);
 			Thread t = new Thread(server);
 			t.start();
-			p = Runtime.getRuntime().exec("java -jar "+jarPath+" "+start+" "+end+" "+" "+host+" "+port+" "+savePath);
+			String command = "java -jar " + jarPath + " " + start + " " + end
+					+ " " + " " + host + " " + port + " " + savePath;
+			System.out.println(command);
+			p = Runtime.getRuntime().exec(command);
+			String line;
+			BufferedReader bri = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+			BufferedReader bre = new BufferedReader(new InputStreamReader(
+					p.getErrorStream()));
+			while ((line = bri.readLine()) != null) {
+				System.out.println(line);
+			}
+			bri.close();
+			while ((line = bre.readLine()) != null) {
+				System.out.println(line);
+			}
+			bre.close();
 			p.waitFor();
-			System.out.println("Crawl>"+p.exitValue());
+			System.out.println("Crawl>" + p.exitValue());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -37,7 +55,7 @@ public class Crawl implements Runnable{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
